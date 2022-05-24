56ad04a8a489ad3b3327f826cfea4cc836a64a6b
Felix Brendel <felix@brendel.io>
2022-05-24 13:32:36 +0200

    test

--- README.md
+++ README.md
@@ -5,9 +5,5 @@ Original: larbs.xyz
 usage:
 
 ```sh
-curl -LO larbs.xyz/larbs.sh
-sh larbs.sh \
-  -r https://github.com/FelixBrendel/arch_rice_1 \
-  -p https://raw.githubusercontent.com/FelixBrendel/arch_rice_1/main/progs.csv
-  -b main
+./rice.sh
 ```
---
+++ rice.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+user_name="$( cd "$(dirname "$0")" ; pwd -P )"
+pushd $user_name > /dev/null
+
+#
+#  RUNNING LARBS
+#
+pacman -S --noconfirm archlinux-keyring
+pacman -S --noconfirm --needed pacman-contrib
+
+curl -LO larbs.xyz/larbs.sh
+
+source larbs.sh                                  \
+  -r https://github.com/FelixBrendel/arch_rice_1 \
+  -b main                                        \
+  -p ./progs.csv
+
+user_name=$name #from larbs
+
+#cleanup this repo
+rm -rf /home/$name/rice.sh/
+rm -rf /home/$name/.git/
+
+mkdir /home/$name/code
+
+# installing doom
+git clone --depth 1 https://github.com/doomemacs/doomemacs  /home/$name/.config/emacs
+git clone --depth 1 https://github.com/felixbrendel/.doom.d /home/$name/.config/doom
+/home/$name/.config/emacs/bin/doom install
